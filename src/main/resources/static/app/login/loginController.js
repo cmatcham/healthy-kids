f7474943db4401d5693815a1de51aea88c783d03
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-04-27 13:07:21 +1200

    add new stickers

--- src/main/resources/static/app/login/loginController.js
+++ src/main/resources/static/app/login/loginController.js
@@ -7,14 +7,18 @@ function loginController($rootScope, $http, $location, $cookies, accountService)
 	self.active = true;
 	self.login = login;
 	self.credentials = {};
+	self.submitForm = submitForm;
 
 	var authenticate = function(credentials, callback) {
 		accountService.authenticate(credentials);
 	}
+
+	function submitForm() {
+		$('.login-form__form').submit()
+	}
 	
 	self.credentials = {};
 	function login() {
-		console.log('self logging in');
 		authenticate(self.credentials, function() {
 			if ($rootScope.authenticated) {
 				$location.path("/");
